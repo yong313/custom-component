e095b549692c9681d3f9246dac3c87dfadee2641
가즈아 <wjsdydxo45@naver.com>
2022-02-08 15:11:56 +0900

    수정

--- src/component/ClickToEdit/MyInput.js
+++ src/component/ClickToEdit/MyInput.js
@@ -3,12 +3,11 @@ import styled from "styled-components";
 
 const MyInput = ({ value, handleValueChange }) => {
   const inputEl = useRef(null);
-  const [isEditMode, setEditMode] = useState(false); //edit모드 상태
-  const [newValue, setNewValue] = useState(value); //출력값 상태
+  const [isEditMode, setEditMode] = useState(false);
+  const [newValue, setNewValue] = useState(value);
 
   useEffect(() => {
     if (isEditMode) {
-      //edit모드가 활성화 되면 input창에 포커스를 줘서 수정이 가능하도록 함
       inputEl.current.focus();
     }
   }, [isEditMode]);
@@ -18,20 +17,16 @@ const MyInput = ({ value, handleValueChange }) => {
   }, [value]);
 
   const handleClick = () => {
-    //span태그를 클릭하면 edit모드가 활성화 되고 위의 useEffect에 의해 input창에 포커싱
     setEditMode(true);
   };
 
   const handleBlur = () => {
-    //input창이 아닌 다른 곳을 클릭하면 edit모드를 비활성화로 만든다.
     setEditMode(false);
-    handleValueChange(newValue); //그리고 input창에 입력되어있는 값으로 newValue를 변경
+    handleValueChange(newValue);
   };
 
   const handleInputChange = (e) => {
-    setNewValue(e.target.value); //input에 입력한 값을 newValue에 담아둠
-    //여기서 입력을 해준다고 바로바로 밑의 출력값이 변화 x
-    //왜냐하면 handleBlur에 의해서 handleValueChange 함수가 실행되어야 값이 바뀌기 때문
+    setNewValue(e.target.value);
   };
 
   return (
